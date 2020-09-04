e3985dadf3df4eff80c144c216d8dca5ef66df9a
伊神紗帆 <>
2020-09-05 01:43:07 +0900

    ファイル書き換え

---
+++ api.php
@@ -0,0 +1,31 @@
+<?php
+
+require "tasks.php";
+$db = new PDO('mysql:host=localhost;dbname=sampleDB;charset=utf8mb4', 'root', '');
+$httpdmethod = $_SERVER['REQUEST_METHOD'];
+
+//echo $httpdmethod;
+
+switch ($httpdmethod) {
+    case 'GET':
+        if(preg_match('/[a][p][i]\/[0-9]/', $_SERVER["REQUEST_URI"])) {
+            $id = preg_replace('/[^0-9]/', '', $_SERVER["REQUEST_URI"]);
+            getTask($db,$id);
+        } elseif($_SERVER["REQUEST_URI"] == '/api') {
+            getTasks($db);
+        }
+        break;
+    case 'POST':
+        createTask($db);
+        break;
+    case 'PUT':
+        echo "PUT";
+        $id = preg_replace('/[^0-9]/', '', $_SERVER["REQUEST_URI"]);
+        updateTask($db,$id);
+        break;
+    case 'DELETE':
+        echo "DELETE";
+        $id = preg_replace('/[^0-9]/', '', $_SERVER["REQUEST_URI"]);
+        deleteTask($db,$id);
+        break;
+}
--- index.php
+++ index.php
@@ -1,30 +1,7 @@
-<?php
-
-require "tasks.php";
-$db = new PDO('mysql:host=localhost;dbname=sampleDB;charset=utf8mb4', 'root', '');
-$httpdmethod = $_SERVER['REQUEST_METHOD'];
-
-switch ($httpdmethod) {
-    case 'GET':
-        if(preg_match('/[a][p][i]\/[0-9]/', $_SERVER["REQUEST_URI"])) {
-            $id = preg_replace('/[^0-9]/', '', $_SERVER["REQUEST_URI"]);
-            getTask($db,$id);
-        } elseif($_SERVER["REQUEST_URI"] == '/api') {
-            getTasks($db);
-        }
-        break;
-    case 'POST':
-        createTask($db);
-        break;
-    case 'PUT':
-        $id = preg_replace('/[^0-9]/', '', $_SERVER["REQUEST_URI"]);
-        updateTask($db,$id);
-        break;
-    case 'DELETE':
-        $id = preg_replace('/[^0-9]/', '', $_SERVER["REQUEST_URI"]);
-        deleteTask($db,$id);
-        break;
-}
+<?php require "api.php"?>
+<html>
+<a href="http://localhost:3000/api">AllTask</a>
+</html>
 
 
 
